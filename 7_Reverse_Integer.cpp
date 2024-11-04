1489f80d852b4d7fe80ef3b101683c5ed381c99a
Abhinav Anand <abhinavforever21@gmail.com>
2024-11-05 00:46:32 +0530

    Question no. is added

---
+++ 7_Reverse_Integer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Solution{
+public:
+    int reverse(int x){
+        int ans = 0;
+        while (x!=0)
+        {
+            int digit = x%10;
+            if (ans > INT_MAX/10 || ans < INT_MIN/10)
+            {
+                return 0;
+            }
+            ans = ans*10 + digit;
+            x = x/10;
+        }
+        return ans;
+    }
+};
+int main()
+{
+    Solution sol;
+    int val = sol.reverse(1534235189);
+    cout << val << endl;
+    return 0;
+}
