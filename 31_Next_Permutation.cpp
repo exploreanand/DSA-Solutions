a99f488dc37c466011bf93dbb93994641471af6e
Abhinav Anand <abhinavforever21@gmail.com>
2025-04-07 02:37:48 +0530

    Solved Leetcode Que 73

--- 31_Next_Permutation.cpp
+++ 31_Next_Permutation.cpp
@@ -34,15 +34,15 @@ int main()
 {
     Solution s;
     vector<int> nums;
-    // nums.push_back(5);
-    // nums.push_back(4);
-    // nums.push_back(7);
-    // nums.push_back(5);
-    // nums.push_back(3);
-    // nums.push_back(2);
+    nums.push_back(5);
+    nums.push_back(4);
+    nums.push_back(7);
+    nums.push_back(5);
     nums.push_back(3);
     nums.push_back(2);
-    nums.push_back(1);
+    // nums.push_back(3);
+    // nums.push_back(2);
+    // nums.push_back(1);
     s.nextPermutation(nums);
     for (int i = 0; i < nums.size(); i++)
     {
---
+++ 73_Set_Matrix_Zeros.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+  void setZeroes(vector<vector<int>> &matrix)
+  {
+    int m = size(matrix);
+    int n = size(matrix[0]);
+    vector<pair<int, int>> rc;
+    for (int i = 0; i < m; i++)
+    {
+      int j = 0;
+      do {
+        if (matrix[i][j] == 0)
+        {
+          rc.emplace_back(i,j);
+        }
+        j++;
+      }
+      while (j < n);
+    }
+    for (auto &p: rc){
+      int row = p.first;
+      int col = p.second;
+      for (int i = 0; i < n; i++)
+      {
+        matrix[row][i] = 0;
+      }
+      for (int i = 0; i < m; i++)
+      {
+        matrix[i][col] = 0;
+      }
+    }
+  }
+};
+int main()
+{
+  Solution obj;
+  vector<vector<int>> matrix(3, vector<int>(3, 0));
+  matrix[0][0] = 1;
+  matrix[0][1] = 1;
+  matrix[0][2] = 1;
+  matrix[1][0] = 1;
+  matrix[1][1] = 0;
+  matrix[1][2] = 1;
+  matrix[2][0] = 1;
+  matrix[2][1] = 1;
+  matrix[2][2] = 1;
+  obj.setZeroes(matrix);
+
+  for (int i = 0; i < matrix.size(); i++)
+  {
+    cout<<"[";
+    for (int j = 0; j < matrix[0].size(); j++)
+    {
+      cout<<matrix[i][j];
+    }
+    cout<<"]";
+
+  }
+  cout<<endl;
+
+  return 0;
+}
