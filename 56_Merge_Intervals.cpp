a0c6b995342607b15a85fe687807ae4038d0b0e1
Abhinav Anand <abhinavforever21@gmail.com>
2026-06-12 11:55:16 +0530

    Cleaned up Que no. 56

--- 56_Merge_Intervals.cpp
+++ 56_Merge_Intervals.cpp
@@ -5,12 +5,13 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.empty()) return {};
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
         int start = intervals[0][0];
         int end = intervals[0][1];
-        for(int i = 0; i<n; i++){
+        for(int i = 1; i<n; i++){
             if(intervals[i][0]<=end){
                 if(intervals[i][1]>end){
                     end = intervals[i][1];
