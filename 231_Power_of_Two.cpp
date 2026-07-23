631b3590972f2f9a62022ae2d1b1431beb3b796f
Abhinav Anand <abhinavforever21@gmail.com>
2026-07-23 05:39:20 +0530

    solved que 231 by bit manipulation

---
+++ 231_Power_of_Two.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    
+    bool isPowerOfTwo(int n) {
+        if(n > 0 && (n & (n - 1)) == 0){
+            return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
