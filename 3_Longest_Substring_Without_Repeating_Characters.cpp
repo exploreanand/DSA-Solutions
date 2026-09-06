81efc109c16c42681ff419229177130b17b18991
Abhinav Anand <abhinavforever21@gmail.com>
2026-09-06 16:31:52 +0530

    Splved Que 3

--- 3_Longest_Substring_Without_Repeating_Characters.cpp
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        int n = s.size();
+        if(n == 0){return 0;}
+        vector<int> last(256, -1);
+
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+
+        while(j < n){
+            i = max(i, last[s[j]] + 1);
+
+            last[s[j]] = j;
+                j++;
+
+            int x = j - i;
+            if(x > ans){
+                ans = x;
+            }
+        }
+        return ans;
     }
 };
 
