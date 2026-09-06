25b17c4bc13ac1a18d8e6a5a30864468141242dd
Abhinav Anand <abhinavforever21@gmail.com>
2026-09-07 04:18:56 +0530

    Solved 424

--- 424_Longest_Repeating_Character_Replacement.cpp
+++ 424_Longest_Repeating_Character_Replacement.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        
+
+        int n = s.size();
+        int freq[26] = {0};
+        int i = 0;
+        int j = 0;
+
+        int m_len = 0;
+        int m_freq = 0;
+
+        while(j < n){
+            freq[s[j] - 'A']++;
+            m_freq = max(m_freq, freq[s[j] - 'A']);
+            
+            while((j - i + 1) - m_freq > k){
+                freq[s[i] - 'A']--;
+                i++;
+            }
+            m_len = max(m_len, j - i + 1);
+            j++;
+        }
+        return m_len;
     }
 };
 
