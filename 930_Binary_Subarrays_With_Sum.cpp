50ab63052bc2284255e999aa025da7420aa826c6
Abhinav Anand <abhinavforever21@gmail.com>
2026-09-07 23:51:31 +0530

    Solved Que 930

---
+++ 930_Binary_Subarrays_With_Sum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int t_sub(vector<int>& nums, int goal){
+        if(goal < 0){return 0;}
+
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        int sum = 0;
+
+        while(j < n){
+            sum += nums[j];
+            while(sum > goal){
+                sum -= nums[i];
+                i++;
+            }
+            ans = ans + (j - i + 1);
+            j++;
+        }
+        return ans;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return t_sub(nums, goal) - t_sub(nums, goal - 1);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
