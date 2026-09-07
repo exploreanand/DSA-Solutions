24fe01e0814f62f33f3264d36f975b90b4d177b4
Abhinav Anand <abhinavforever21@gmail.com>
2026-09-08 01:56:25 +0530

    Solved Que 930

---
+++ 1248_Count_Number_of_Nice_Subarrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
